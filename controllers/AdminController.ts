4e5db1df45a77177d4dd572623ed0a45dbee84c8
chidozavamed <chido.ukaigwe@zavamed.com>
2021-10-22 16:52:28 -0600

    create vendor, get a list of vendors, get vendor by id git add .

--- controllers/AdminController.ts
+++ controllers/AdminController.ts
@@ -3,11 +3,20 @@ import { CreateVendorInput } from '../dto'
 import { Vendor } from '../models'
 import { GeneratePassword, GenerateSalt } from '../utility'
 
+//  email is optional
+export const FindVendor = async (id: string | undefined, email?: string) => {
+    if (email) {
+        return await Vendor.findOne({email:email})
+    }else{
+        return await Vendor.findById(id)
+    }
+}
+
 export const CreateVendor = async (req:Request, res:Response, next:NextFunction) => {
  
     const {name, address, pincode, foodType, email, password, ownerName, phone } = <CreateVendorInput>req.body
 
-    const existingVendor = await Vendor.findOne({email: email})
+    const existingVendor = await FindVendor('', email)
 
     if(existingVendor !== null){
         return res.json({"message": "A vendor already exists with this email address"})
@@ -39,10 +48,23 @@ export const CreateVendor = async (req:Request, res:Response, next:NextFunction)
 
 export const GetVendors = async (req:Request, res:Response, next:NextFunction) => {
  
-    
+    const vendors = await Vendor.find()
+
+    if(vendors !== null){
+        return res.json(vendors)
+    }
+
+    return res.json({"message": "vendors data not available"})
     
 }
 
 export const GetVendorByID = async (req: Request, res: Response, next: NextFunction) => {
+    const vendorId = req.params.id
+    const vendor = await FindVendor(vendorId)
 
+    if(vendor !== null){
+        return res.json(vendor)
+    }
+
+    return res.json({"message": "vendors data not available"})
 }
